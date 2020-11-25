883409690b7eb6a0a47691bf96d2860f5d92fe18
Sanzay Dahal <sanzaydahal12345@gmail.com>
2020-11-25 23:39:33 +1100

    nav bar, toogle and style

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
+import './App.css';
 
 import HomeScreen from './screens/HomeScreen';
 import RoomsScreen from './screens/RoomsScreen';
 import SingleRoomScreen from './screens/SingleRoomScreen';
 import ErrorScreen from './screens/ErrorScreen';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Switch>
         <Route exact path='/' component={HomeScreen} />
         <Route exact path='/rooms' component={RoomsScreen} />
