bdb876cc33d21ce8082ca299656758dd9685fb30
Sanzay Dahal <sanzaydahal12345@gmail.com>
2020-11-26 22:35:04 +1100

    Nodemon and concurrently setup with script

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const defaultImage = '/images/room-1.jpeg';
 
 const Room = ({ room }) => {
   const { name, id, images, price } = room;
@@ -7,7 +10,7 @@ const Room = ({ room }) => {
   return (
     <article className='room'>
       <div className='img-container'>
-        <img src={images[0].url} alt='single room' />
+        <img src={images[0].url || defaultImage} alt='single room' />
         <div className='price-top'>
           <h6>${price}</h6>
           <p>per night</p>
@@ -15,9 +18,19 @@ const Room = ({ room }) => {
         <Link to={`/rooms/${id}`} className='btn-primary room-link'>
           Features
         </Link>
+        <p className='room-info'>{name}</p>
       </div>
     </article>
   );
+};
+
+Room.propTypes = {
+  room: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(PropTypes.object).isRequired,
+    price: PropTypes.number.isRequired,
+  }),
 };
 
 export default Room;
