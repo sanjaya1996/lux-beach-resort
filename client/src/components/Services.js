e40ef3e6e335205809a2005c8946b46b98695322
Sanzay Dahal <sanzaydahal12345@gmail.com>
2020-11-26 11:53:32 +1100

    Add Services and install redux dependencies

---
+++ client/src/components/Services.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from 'react-icons/fa';
+
+import Title from './Title';
+
+const services = [
+  {
+    icon: <FaCocktail />,
+    title: 'Free Cocktails',
+    info:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias molestias eius libero?',
+  },
+  {
+    icon: <FaHiking />,
+    title: 'Endless Hiking',
+    info:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias molestias eius libero?',
+  },
+  {
+    icon: <FaShuttleVan />,
+    title: 'Free Shuttle',
+    info:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias molestias eius libero?',
+  },
+  {
+    icon: <FaBeer />,
+    title: 'Strongest Beer',
+    info:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias molestias eius libero?',
+  },
+];
+
+const Services = () => {
+  return (
+    <section className='services'>
+      <Title title='services' />
+      <div className='services-center'>
+        {services.map((item, idx) => (
+          <article key={idx} className='service'>
+            <span>{item.icon}</span>
+            <h6>{item.title}</h6>
+            <p>{item.info}</p>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Services;
--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -2,16 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
+import Services from '../components/Services';
 
 const HomeScreen = () => {
   return (
-    <Hero>
-      <Banner title='luxurious rooms' subtitle='deluxe rooms starting at $299'>
-        <Link to='/rooms' className='btn-primary'>
-          Our rooms
-        </Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero>
+        <Banner
+          title='luxurious rooms'
+          subtitle='deluxe rooms starting at $299'
+        >
+          <Link to='/rooms' className='btn-primary'>
+            Our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+    </>
   );
 };
 
