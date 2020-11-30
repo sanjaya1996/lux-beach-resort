6d368ef38795ffaf6b976f53059160991179a7dc
Sanzay Dahal <sanzaydahal12345@gmail.com>
2020-12-01 01:01:44 +1100

    Mysql database, phpmyAdmin connected

--- server/server.js
+++ server/server.js
@@ -1,11 +1,37 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const ROOMS = require('./data/data');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'luxbeachresort',
+});
+
+// Connect
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log('Mysql Connected...');
+});
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('API is running...');
+});
+
+// Create DB
+app.get('/createdb', (req, res) => {
+  let sql = 'CREATE DATABASE luxBeachResort';
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send('Database Created');
+  });
 });
 
 app.get('/api/rooms', (req, res) => {
