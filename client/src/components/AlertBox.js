0527a361cc4127b9a44c6c02cb3663bfdb0d4a55
Sanzay Dahal <sanzaydahal12345@gmail.com>
2020-12-16 21:45:55 +1100

    Create fully customized Alert box

--- client/src/components/AlertBox.js
+++ client/src/components/AlertBox.js
@@ -1,17 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Styled from 'styled-components';
 
-const AlertBox = () => {
+// #4BB543 primary color
+const StyledAlert = Styled.div`
+position: fixed;
+top: 80px;
+right: 20px;
+left: 20px;
+margin: auto;
+width: 80vw;
+max-width: 1000px;
+text-align: center;
+border-radius: 10px;
+padding: 20px;
+background-color: ${(props) =>
+  props.type === 'error' ? '#cd472b' : '#4BB543'} ; 
+color: white;
+margin-bottom: 15px;
+display: ${(props) => (props.clicked ? 'none' : null)}
+`;
+
+const Closebtn = Styled.span`
+position: fixed;
+top: 5px;
+right: 10px;
+font-size: 1.2em;
+margin-left: 15px;
+float: right;
+line-height: 20px;
+cursor: pointer;
+transition: 0.3s;
+:hover {
+  color: black;
+}
+
+`;
+
+const StyledParagraph = Styled.p`
+margin: auto;
+letter-spacing: 3px;
+`;
+
+const AlertBox = ({ message, type }) => {
+  const [alertClosed, setAlertClosed] = useState(false);
   return (
-    <div className='error-card'>
-      <h4>
-        Error{' '}
-        <span>
-          <strong>!!</strong>
-        </span>{' '}
-        while loading Rooms....
-      </h4>
-    </div>
+    <StyledAlert type={type} clicked={alertClosed}>
+      <StyledParagraph>
+        {message}
+        <Closebtn onClick={() => setAlertClosed(true)}>x</Closebtn>
+      </StyledParagraph>
+    </StyledAlert>
   );
+};
+
+AlertBox.defaultProps = {
+  type: 'error',
 };
 
 export default AlertBox;
--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -5,10 +5,15 @@ import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import Services from '../components/Services';
 import FeaturedRooms from '../components/FeaturedRooms';
+import AlertBox from '../components/AlertBox';
 
 const HomeScreen = () => {
   return (
     <>
+      <AlertBox
+        type='primary'
+        message='Error While loading rooms Error While loading rooms Error While loading rooms Error While loading rooms'
+      />
       <Hero>
         <Banner
           title='luxurious rooms'
