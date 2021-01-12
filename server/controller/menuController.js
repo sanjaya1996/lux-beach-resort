0312e0473743ef378d231008110276bc0e72c37c
Sanzay Dahal <sanzaydahal12345@gmail.com>
2021-01-12 17:36:07 +1100

    Backend- menuRoutes: Get a meal by Id, Create a new meal & Update a meal by Id complete

--- server/controller/menuController.js
+++ server/controller/menuController.js
@@ -47,4 +47,121 @@ const deleteMeal = async (req, res, next) => {
   }
 };
 
-module.exports = { getMeals, getMealById, deleteMeal };
+// @desc    Create a new meal
+// @route   POST /api/menu/:id
+// @access  Private/Admin
+const createMeal = async (req, res, next) => {
+  try {
+    const {
+      name,
+      category,
+      price,
+      imageUrl,
+      duration,
+      isGlutenFree,
+      isVegan,
+      isVegeterian,
+      isLactoseFree,
+      ingredients,
+    } = req.body;
+
+    const query = {
+      text: `INSERT INTO meals 
+                  (name , category, price, imageurl, duration, is_gluten_free, is_vegan, is_vegeterian, is_lactose_free, ingredients)
+              VALUES 
+                  ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *
+              `,
+      values: [
+        name,
+        category,
+        price,
+        imageUrl,
+        duration,
+        isGlutenFree,
+        isVegan,
+        isVegeterian,
+        isLactoseFree,
+        ingredients,
+      ],
+    };
+
+    const results = await db.query(query);
+    res.json(results.rows[0]);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// @desc    Update a meal by Id
+// @route   PUT /api/menu/:id
+// @access  Private/Admin
+const updateMeal = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+
+    const existedMeal = await db.query('SELECT * FROM meals WHERE id = $1', [
+      id,
+    ]);
+
+    if (existedMeal.rowCount === 1) {
+      const meal = existedMeal.rows[0];
+
+      let {
+        name,
+        category,
+        price,
+        imageUrl,
+        duration,
+        isGlutenFree,
+        isVegan,
+        isVegeterian,
+        isLactoseFree,
+        ingredients,
+      } = req.body;
+
+      name = name || meal.name;
+      category = category || meal.category;
+      price = price || meal.price;
+      imageUrl = imageUrl || meal.imageurl;
+      duration = duration || meal.duration;
+      isGlutenFree =
+        isGlutenFree === undefined ? meal.is_gluten_free : isGlutenFree;
+      isVegan = isVegan === undefined ? meal.is_vegan : isVegan;
+      isVegeterian =
+        isVegeterian === undefined ? meal.is_vegeterian : isVegeterian;
+      isLactoseFree =
+        isLactoseFree === undefined ? meal.is_lactose_free : isLactoseFree;
+      ingredients = ingredients || meal.ingredients;
+
+      const query = {
+        text: `UPDATE meals SET 
+              name = $1, category= $2, price= $3, imageurl= $4, duration= $5, 
+              is_gluten_free= $6, is_vegan= $7, is_vegeterian= $8, is_lactose_free= $9, ingredients= $10
+           WHERE id = $11 RETURNING *`,
+        values: [
+          name,
+          category,
+          price,
+          imageUrl,
+          duration,
+          isGlutenFree,
+          isVegan,
+          isVegeterian,
+          isLactoseFree,
+          ingredients,
+          meal.id,
+        ],
+      };
+
+      const results = await db.query(query);
+      res.json(results.rows[0]);
+    } else {
+      res.status(404);
+      throw new Error('Meal Not found');
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { getMeals, getMealById, deleteMeal, createMeal, updateMeal };
--- server/routes/menuRoutes.js
+++ server/routes/menuRoutes.js
@@ -4,11 +4,17 @@ const {
   getMeals,
   getMealById,
   deleteMeal,
+  updateMeal,
+  createMeal,
 } = require('../controller/menuController');
 
 const router = express.Router();
 
-router.get('/', getMeals);
-router.route('/:id').get(getMealById).delete(checkAdmin, deleteMeal);
+router.route('/').get(getMeals).post(checkAdmin, createMeal);
+router
+  .route('/:id')
+  .get(getMealById)
+  .delete(checkAdmin, deleteMeal)
+  .put(checkAdmin, updateMeal);
 
 module.exports = router;
