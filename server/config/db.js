a1b26ad7a6199499ec535b0d47be9e4b5f50c92c
Sanzay Dahal <sanzaydahal12345@gmail.com>
2021-03-08 09:05:58 +1100

    Bug fix-2 database connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,15 @@
 const { Pool } = require('pg');
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const prodConfig = {
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 
-const pool =
-  process.env.NODE_ENV === 'production' ? new Pool(prodConfig) : new Pool();
+const pool = isProduction ? new Pool(prodConfig) : new Pool();
 
 module.exports = {
   query: (text, values) => pool.query(text, values),
